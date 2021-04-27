722df3aef8a661f0be26f662acca37edf028eff1
Peace1Duke <62473847+Peace1Duke@users.noreply.github.com>
2021-04-27 20:35:40 +0300

    new functional

--- mainmodule.py
+++ mainmodule.py
@@ -57,8 +57,8 @@ def exit():
 
 #the actual search function
 def search(arg):
-	country = str(input(f"""{gr}>Input the country you are interested in(just Enter for all countries): """))
-	if country == "  ":
+	country = str(input(f"""{gr}>Input the country you are interested in('*' for all countries): """))
+	if country == "*":
 		...
 	else: 
 		arg = arg +',country:' + country
@@ -67,7 +67,7 @@ def search(arg):
 		results = api.search(arg)
 
         # Show the results
-                #print('Results found: {}'.format(results['total']))
+		print('Results found: {}'.format(results['total']))
 		for result in results['matches']:
 				print('IP: {}'.format(result['ip_str']))
 				print(result['data'])
@@ -77,36 +77,159 @@ def search(arg):
 		print(f"""{re}Api Error """)
 		exit()
 
+#cams
+def goahead():
+	print(f"""{re}!exploit with /system.ini?loginuse&loginpas ! """)
+	search('realm="GoAhead", domain=":81"')
 
-def cam():
-	print(f"""{re}!exploit with /system.ini?loginuse&loginpas! """)
-	search('realm="GoAhead", domain=":81"') 
-	
+def rtsp():
+	print(f"""{re}!exploit with rtsp://XXX.XXX.XXX.XXX + public comand(check result) ! """)
+	search('port:554 has_screenshot:true')
 
+def yawcams():
+	search('"Server: yawcam" "Mime-Type: text/html"')
+
+def android_web():
+	search('"Server: IP Webcam Server" "200 OK"')
+
+def dvrs():
+	search('html:"DVR_H264 ActiveX"')
+
+def webcamxp7():
+	search('("webcam 7" OR "webcamXP") http.component:"mootools" -401')
+
+#industrial 
+def samsung_billboards():
+	search('"Server: Prismview Player"')
+
+def gas_spc():
+	search('"in-tank inventory" port:10001')
+
+def alpr():
+	search('P372 "ANPR enabled"')
+
+def traffic_lc():
+	search('mikrotik streetlight')
+
+def tesla_chargstatus():
+	search('http.title:"Tesla PowerPack System" http.component:"d3" -ga3ca4f2')
+
+#remote acces
+def adb_phone():
+	print(f"""{re}! use with PhoneSploit ! """)
+	search('android debug bridge product:”Android Debug Bridge”')
+
+def vnc():
+	search('"authentication disabled" "RFB 003.008"')
+
+#ftp
 def asus_ftp():
-	print(f"""{re}! exploit withftp://XXX.XXX.XXX.XXX! """)
-	search('asus 230 port:"21"') 
+	print(f"""{re}! exploit withftp://XXX.XXX.XXX.XXX ! """)
+	search('asus 230 port:21') 
 	
+def ftp():
+	search('"220" "230 Login successful." port:21')
 
+#printers
+def hprinters():
+	search('"Serial Number:" "Built:" "Server: HP HTTP"')
+
+def canon1():
+	search('"Server: KS_HTTP" "200 OK"')
+def canon2():
+	search('"Server: CANON HTTP Server"')
+
+def xerox():
+	search('ssl:"Xerox Generic Root"')
+#other
 def hacked_site():
 	search('http.title:"hacked by"')
+
+def octoprint_control():
+	search('title:"OctoPrint" -title:"Login" http.favicon.hash:1307375944')
+
+def etheriuminers():
+	search('"ETH - Total speed"')
+
+def north_korea():
+	search('net:175.45.176.0/22,210.52.109.0/24,77.94.35.0/24')
+
+def chromecast():
+	search('"Chromecast:" port:8008')
+
+
+
 
 #list function 
 
 def list():
 	print(f"""
-{gr}1: cam,
-2: asus_ftp,
-3: hacked_site
+{cy}!Cameras!
+{gr}1: GoAhead cams
+2: RTSP webcams
+3: Yawcams
+4: Android IP Webcam Server  	
+5: Security DVRs
+6: webcamXP/webcam7
+
+{cy}!Industrial Control Systems!
+{gr}7: Samsung Electronic Billboards
+8: Gas Station Pump Controllers
+9: Automatic License Plate Readers
+10: Traffic Light Controllers / Red Light Cameras
+11: Tesla PowerPack Charging Status
+
+{cy}!Remote Desktop and Phone!
+{gr}12: ADB Phone
+13: Unprotected VNC
+
+{cy}!FTP!
+{gr}14: ASUS FTP Servers with Anonymous Login
+15: FTP Servers with Anonymous Login
+
+{cy}!Printers & Copiers!
+{gr}16: HP Printers
+17: Canon Printers 1
+18: Canon Printers 2
+19: Xerox Copiers/Printers
+
+{cy}!Other!
+{gr}20: Hacked Site
+21: OctoPrint 3D Printer Controllers
+22: Etherium Miners
+23: Literally Everything in North Korea 
+24:Chromecasts / Smart TVs
+
     	""")
 	numeth = int(input(">Input the number of the method you need:"))
 	choose_list(numeth)
 
 def choose_list(arg):
     switcher = {
-        1: cam,
-        2: asus_ftp,
-        3: hacked_site
+        1: goahead ,
+        2: rtsp,
+        3: yawcams,
+        4: android_web,
+        5: dvrs,
+        6: webcamxp7,
+        7: samsung_billboards,
+        8: gas_spc,
+        9: alpr,
+        10: traffic_lc,
+        11: tesla_chargstatus,
+        12: adb_phone,
+        13: vnc,
+        14: asus_ftp,
+        15: ftp,
+        16: hprinters,
+        17: canon1,
+        18: canon2,
+        19: xerox,
+        20: hacked_site,
+        21: octoprint_control,
+        22: etheriuminers,
+        23: north_korea,
+        24: chromecast
     }
     non_existent_arg = switcher.get(arg, lambda:"Invalid number!")
     print (non_existent_arg())
